d2a9c1badeaf0265701ea87b9f8825396f2bdf23
Trisha <johndoe@example.com>
2025-04-07 16:07:39 +0600

    added user experipence

--- src/index.js
+++ src/index.js
@@ -11,12 +11,18 @@ function formSubmit(event) {
   event.preventDefault();
 
   let apiKey = "3od7af2214ca31898090dtcb837a2247";
-  let prompt = "Create a story based on users demand.";
-  let usersDemand = document.querySelector("#input-txt");
 
-  let context = `You are a story generator AI, you create story based on ${usersDemand.value} demand on various topics.note that you will make 5 lines short story.the story must contain important characters and dialogues of those characters also the story must give a good message for society.no vulnerability will accept.no adult content will add here.you should always mention the story name on your story based on users demand.you should not write more than 5 lines`;
+  let usersDemand = document.querySelector("#input-txt");
+  let prompt = `Create a story based on ${usersDemand.value} demand.`;
+
+  let context =
+    "You are a story generator AI, you create story based on users demand on various topics.note that you will make 5 lines short story.the story must contain important characters and dialogues of those characters also the story must give a good message to the society.no vulnerability will accept.no adult content will add here.you should always give a story name on the top of your story based on users demand.you should not write more than 5 lines";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let storyBuilding = document.querySelector("#story");
+  storyBuilding.classList.remove("hidden");
+  storyBuilding.innerHTML = `<div class="wait">⏳Please wait, your required story is generating.. </div>`;
 
   axios.get(apiUrl).then(createStory);
 }
